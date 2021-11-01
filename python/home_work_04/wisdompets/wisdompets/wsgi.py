7a2c0ac24b771d722fcb3aedbb298135d2121092
Andrey Lavrentiev <aaa.lavrentev@gmail.com>
2021-11-01 20:21:48 +1000

    Delete python/home_work_04/wisdompets directory

--- python/home_work_04/wisdompets/wisdompets/wsgi.py
+++
@@ -1,7 +0,0 @@
-import os
-import sys
-sys.path.append('/home/bitnami/django_projects/wisdompets')
-os.environ.setdefault("PYTHON_EGG_CACHE", "/home/bitnami/django_projects/wisdompets/egg_cache")
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wisdompets.settings")
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
